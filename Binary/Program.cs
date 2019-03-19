4b238da5d7fd276a5182360117f6ab42ef1be7bd
laster40 <laster40@naver.com>
2019-03-19 17:55:22 +0900

    first

---
+++ Binary/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Man c = new Man();
+            c.hair = 1;
+            c.face = 2;
+            c.nose = 3;
+            c.lip = 4;
+
+            byte[] bin1 = c.Write();
+
+            Man c1 = new Man();
+            c1.Read(bin1);
+        }
+    }
+
+    public class Man
+    {
+        public byte hair;
+        public byte face;
+        public byte nose;
+        public byte lip;
+
+        public void Read(byte[] bin)
+        {
+            BinaryReader reader = new BinaryReader(bin);
+            hair = (byte)reader.ReadInt(5);
+            face = (byte)reader.ReadInt(5);
+            nose = (byte)reader.ReadInt(5);
+            lip = (byte)reader.ReadInt(5);
+            
+        }
+
+        public byte[] Write()
+        {
+            BinaryWriter writer = new BinaryWriter(16);
+            writer.WriteInt(hair, 5);
+            writer.WriteInt(face, 5);
+            writer.WriteInt(nose, 5);
+            writer.WriteInt(lip, 5);
+            return writer.Buffer;
+        }
+    }
+
+
+}
---
+++ BinaryCxx/BinaryCxx.cpp
@@ -0,0 +1,24 @@
+﻿// BinaryCxx.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <iostream>
+
+#include "Packet.h"
+
+int main()
+{
+	PacketWriter Writer;
+
+	Writer.WriteBits(1, 1);
+	Writer.WriteBits(2, 2);
+	Writer.WriteBits(3, 3);
+	Writer.WriteBits(4, 4);
+
+	PacketReader Reader(Writer);
+	int a = Reader.ReadBits(1);
+	int b = Reader.ReadBits(2);
+	int c = Reader.ReadBits(3);
+	int d = Reader.ReadBits(4);
+
+}
